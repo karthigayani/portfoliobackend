17c561cfa081e18facbcaba6ebce1bd4d5444b20
karthigayani <karthi1826@gmail.com>
2024-01-06 01:39:47 +0530

    PORT hidden

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use("/", router);
-app.listen(4000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 // console.log(process.env.USER);
 // console.log(process.env.PASSWORD);
 
